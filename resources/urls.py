2153cafdd67dca8573435484bc98af70d5dd9f38
arthurW1935 <sourashis.23bcs10038@ms.sst.scaler.com>
2023-09-24 22:47:55 +0530

    changed file structures

--- resources/urls.py
+++ resources/urls.py
@@ -7,7 +7,14 @@ urlpatterns = [
     path('python/', views.python, name='python'),
     path('java/', views.java, name='java'),
     path('cpp/', views.cpp, name='cpp'),
+    path('html/', views.html, name='html'),
+    path('css/', views.css, name='css'),
+    path('javascript/', views.javascript, name='javascript'),
+    path('c', views.c, name='c'),
+    path('csharp/', views.csharp, name='csharp'),
     path('webdev/', views.webdev, name='webdev'),
+    path('datascience/', views.datascience, name='datascience'),
     path('machinelearning/', views.machinelearning, name='machinelearning'),
+    path('appdev/', views.appdev, name='appdev'),
     path('about/', views.about, name="about")
 ]
--- resources/views.py
+++ resources/views.py
@@ -9,19 +9,40 @@ def home(request):
     return render(request, 'home.html')
 
 def python(request):
-    return render(request, 'python.html')
+    return render(request, 'languages/python.html')
 
 def java(request):
-    return render(request, 'java.html')
+    return render(request, 'languages/java.html')
 
 def cpp(request):
-    return render(request, 'cpp.html')
+    return render(request, 'languages/cpp.html')
+
+def html(request):
+    return render(request, 'languages/html.html')
+
+def css(request):
+    return render(request, 'languages/css.html')
+
+def javascript(request):
+    return render(request, 'languages/javascript.html')
+
+def c(request):
+    return render(request, 'languages/c.html')
+
+def csharp(request):
+    return render(request, 'languages/csharp.html')
 
 def webdev(request):
-    return render(request, 'webdev.html')
+    return render(request, 'topics/webdev.html')
+
+def datascience(request):
+    return render(request, 'topics/datascience.html')
 
 def machinelearning(request):
-    return render(request, 'machinelearning.html')
+    return render(request, 'topics/machinelearning.html')
+
+def appdev(request):
+    return render(request, 'topics/appdev.html')
 
 def about(request):
     if request.method == 'POST':
@@ -31,12 +52,11 @@ def about(request):
             email = form.cleaned_data['email']
             message = f"Thank you for reaching out to us. Your message has been recieved. We will reply shortly.\n\nRecieved from {name}<{email}>\nMessage:\n{form.cleaned_data['message']}"
 
-            # Send email
             send_mail(
                 f'Contact Form Submission from {name}',
                 f'Name: {name}\nEmail: {email}\nMessage: {message}',
-                settings.EMAIL_HOST_USER,  # Replace with your 'from' email
-                [email],  # Replace with a list of recipient emails
+                settings.EMAIL_HOST_USER, 
+                [email], 
                 fail_silently=False,
             )
             return render(request, 'about.html', {'form': ContactForm(), 'name': name})
