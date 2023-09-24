bf3c5859d618fa4590e26ee9b80fb426ea7170ff
arthurW1935 <sourashis.23bcs10038@ms.sst.scaler.com>
2023-09-25 00:24:55 +0530

    allowed vercel as a host

--- skill_overflow/settings.py
+++ skill_overflow/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_rd1e1@v$1_ldf3+ok65u3iqe5$hyc!1s_r-_&_yxn!&rnb=1p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
